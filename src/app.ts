e3911d285de03b1f3695b1f379b098c8a594c582
Boran Seckin <me@boranseckin.com>
2020-03-23 18:04:54 -0400

    Remove ssh tunnel

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import dotenv from 'dotenv';
+import connectDB from './db';
 
 dotenv.config();
 
-require('./db');
-require('./server');
+const fallback = setTimeout(() => {
+  throw new Error('Cannot connect to the database!');
+}, 5000);
+
+connectDB()
+  .then(() => {
+    clearTimeout(fallback);
+    // eslint-disable-next-line global-require
+    require('./server');
+  })
+  .catch((error) => console.log(error));
--- src/db.ts
+++ src/db.ts
@@ -1,51 +1,22 @@
-import fs from 'fs';
 import mongoose from 'mongoose';
-import tunnel from 'tunnel-ssh';
 
-const {
-  dbUrl,
-  dbUsername,
-  dbPassword,
-  dbPrivateKey,
-  dbName,
-  dbPassphrase,
-} = process.env;
-
-const config = {
-  host: dbUrl,
-  username: 'root',
-  privateKey: fs.readFileSync(dbPrivateKey),
-  passphrase: dbPassphrase,
-  port: 22,
-  dstHost: '127.0.0.1',
-  dstPort: 27017,
-  localHost: '127.0.0.1',
-  localPort: 27017,
-};
-
-function connectDB(username: string, password: string, name: string) {
+export default function connectDB() {
   return new Promise((resolve, reject) => {
-    mongoose.connect(`mongodb://${username}:${password}@localhost:27017/${name}`, {
+    const {
+      dbUrl,
+      dbPort,
+      dbUsername,
+      dbPassword,
+      dbName,
+    } = process.env;
+
+    mongoose.connect(`mongodb://${dbUsername}:${dbPassword}@${dbUrl}:${dbPort}/${dbName}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      authSource: name,
+      authSource: dbName,
     })
       .then(resolve)
       .catch((error) => reject(error));
   });
 }
-
-tunnel(config, (err) => {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  console.log('SSH Tunnel connected!');
-  connectDB(dbUsername, dbPassword, dbName)
-    .then(() => {
-      console.log('DB connected!');
-    })
-    .catch((error) => console.log(error));
-});
