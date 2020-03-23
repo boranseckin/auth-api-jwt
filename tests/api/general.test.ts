44fde6ee7c9572fd720c5d54e3af707d706be7f2
Boran Seckin <me@boranseckin.com>
2020-03-22 21:48:31 -0400

    Add 503 error test

--- tests/api/general.test.ts
+++ tests/api/general.test.ts
@@ -2,29 +2,47 @@ import request from 'supertest';
 
 import { app, server } from '../../src/server';
 
-afterAll(async () => {
-  server.close();
-  await new Promise((resolve) => setTimeout(() => resolve(), 500)); // avoid jest open handle error
-});
-
 const agent = request.agent(app);
 
+afterAll(async () => {
+  server.close();
+  await new Promise((resolve) => setTimeout(() => resolve(), 500));
+});
+
 describe('General Endpoints', () => {
-  it('/api should return 200', async () => {
+  test('/api should return 200', async () => {
     const res = await agent
       .get('/api');
     expect(res.status).toBe(200);
   });
 
-  it('/teapot should return 418', async () => {
+  test('/teapot should return 418', async () => {
     const res = await agent
       .get('/teapot');
     expect(res.status).toBe(418);
   });
 
-  it('/foo should return 404', async () => {
+  test('/foo should return 404', async () => {
     const res = await agent
       .get('/foo');
     expect(res.status).toBe(404);
   });
+
+  test('/api/auth/signup should return 503 without DB connection', async () => {
+    const username = Math.random().toString(36).substring(5);
+    const password = Math.random().toString(36).substring(5);
+
+    const res = await agent
+      .post('/api/auth/signup')
+      .set('Accept', 'application/json')
+      .send({
+        user: {
+          username,
+          password,
+          email: `${username}@test.com`,
+        },
+      });
+
+    expect(res.status).toBe(503);
+  }, 10000);
 });
